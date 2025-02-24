573d63735e46da61d10526d357037a7939718a53
Brandan Herron <53979709+brandan1989@users.noreply.github.com>
2025-02-24 11:20:13 -0800

    2025 Update
    Updating portfolio website

---
+++ 2025.js
@@ -0,0 +1,17 @@
+// script.js
+
+var i = 0;
+var text = "Brandan Herron";
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 80);  // Speed of typing in milliseconds
+  }
+}
+
+// Run typeWriter after the page fully loads
+window.addEventListener("DOMContentLoaded", () => {
+  typeWriter();
+});
